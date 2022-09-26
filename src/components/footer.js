882c85bab4d883cf8bee0e2fe72fff489c2ece29
travis-mcginley <travis.o.mcginley@gmail.com>
2022-09-26 12:07:25 -0400

    Add company icons and change color scheme

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,35 @@ import React from 'react';
 import '../styles/footer.css';
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="copyright">
+      Copyright &copy; Tracereact, LLC 2022
+      </div>
+      <div className="icon8">
+        Icons Supplied by&nbsp;
+        <span>
+          <a 
+            href="https://icons8.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Icon 8
+          </a>  
+        </span>
+        &nbsp;and&nbsp;
+        <span>
+          <a
+            href="https://fontawesome.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Font Awesome
+          </a>  
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../styles/header.css';
+import Icon from './icon';
 
 const Header = () => {
   return (
     <div id="page-top" className="header">
-      Header
+      <div className="company-logo">
+        <Icon />
+        <div className="company-name">tracereact</div>
+      </div>
     </div>
   );
 };
