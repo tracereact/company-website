be0b3d31ea0898fa8f31d32b26f0d0726e8b0c91
travis-mcginley <travis.o.mcginley@gmail.com>
2022-09-27 12:04:40 -0400

    Adjust styling and add new about section

---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/about.css';
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="title">About</div>
+      <div className="info">Hi there! I&apos;m Travis</div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/content.js
+++ src/components/content.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import About from './about';
 import '../styles/content.css';
 
 const Content = () => {
   return (
-    <div className="content" />
+    <div className="content">
+      <div id="about">
+        <About />
+      </div>
+    </div>
   );
 };
 
