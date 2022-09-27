1dc3b2498a9157bb24d077d7f726f66fc07ac5ec
travis-mcginley <travis.o.mcginley@gmail.com>
2022-09-27 15:40:20 -0400

    Add links to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,6 +7,22 @@ const Footer = () => {
     <div className="footer">
       <div id="contact" className="anchor" />
       <Contact />
+      <div className="links">
+        <a
+          href="https://github.com/tracereact"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="github icon link fa-brands fa-github" />
+        </a>
+        <a
+          href="https://linkedin.com/in/travismcginley"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="linkedin icon link fa-brands fa-linkedin" />
+        </a>
+      </div>
       <div className="copyright">Copyright &copy; Trace React, LLC 2022</div>
       <div className="disclaimer">
         Icons supplied by&nbsp;
