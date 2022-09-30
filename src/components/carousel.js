383f1700e2116139e0f6a343bbc1a95f9dd8326e
travis-mcginley <travis.o.mcginley@gmail.com>
2022-09-30 11:38:36 -0400

    Add ability to move forward while simulating infinite array

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/carousel.css';
 
 const Carousel = () => {
-  const [pos, changePos] = React.useState(0);
-  const carouselItems = document.querySelectorAll('.carousel .item');
+  const [carouselItems, setItems] = useState(null);
+
+  // Gather items once page loads
+  useEffect(() => {
+    setItems(document.querySelectorAll('.carousel .item'));
+  }, []);
+
+  let pos = 0;
+  // const carouselItems = document.querySelectorAll('.carousel .item');
+
+  // Add or remove a clone from either side to simulate infinite scrolling
+  const rotateItems = (dir) => {
+    console.log(`rotating ${dir}...`);
+    
+    if (dir === 'forward') {
+      const tempElement = carouselItems[pos - 1].cloneNode(true);
+      carouselItems[carouselItems.length - 1].after(tempElement);
+      carouselItems[pos - 1].remove();
+
+    } else {
+      const tempElement = carouselItems[pos + 1].cloneNode(true);
+      carouselItems[0].before(tempElement);
+      carouselItems[pos + 1].remove();
+    }
+    setItems(document.querySelectorAll('.carousel .item'));
+  };
 
   const changeItem = () => {
     // Something is wrong with the carousel items list
-    if (!carouselItems || carouselItems.length < 1) { return; }
+    if (!carouselItems || carouselItems.length < 1) { 
+      console.log('Error with items');
+      return; 
+    }
 
     // Helper function to handle negative inputs
     const mod = (a, b) => {
@@ -16,26 +43,40 @@ const Carousel = () => {
 
     const itemNum = mod(pos, carouselItems.length);
     console.log(`Heading to item number ${itemNum}`);
-
+    // console.log(`Pos: ${pos}`);
     carouselItems[itemNum].scrollIntoView ({
       behavior: 'smooth',
       inline: 'start',
       block: 'nearest'
     });
+
   };
 
   // Go to the previous item in the carousel
   const goBack = () => {
-    console.log('Backing up..');
-    changePos(pos - 1);
+
+    console.log('Going back..');
+    pos -= 1;
+
+    // Temp failsafe
+    // if (pos < 0) { pos = 0; return; }
     changeItem();
+
+    rotateItems('back');
   };
 
   // Go to the next item in the carousel
   const goForth = () => {
     console.log('Going up..');
-    changePos(pos + 1);
+    pos += 1;
+
+    // Temp failsafe
+    // if (pos > carouselItems.length - 1) { pos = carouselItems.length - 1; return; }
+
+    // console.log('Going down..');
     changeItem();
+
+    rotateItems('forward');
   }; 
 
   return (
